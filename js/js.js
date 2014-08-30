c0af18fcebd3136ebd6faa48d24383fa250730cf
Gustavo Fernandez <contato@lotus3d.com.br>
2014-08-30 11:56:12 -0300

    Versao 1 do Iron Man CSS3

--- js/js.js
+++ js/js.js
@@ -2,11 +2,15 @@ function fade(btnElement) {
             if (btnElement.value === "OFF") {
                 document.getElementById("eyeRblur").className = "fade-out";
                 document.getElementById("eyeLblur").className = "fade-out";
+                document.getElementById("reactor1").className = "fade-out";
+                document.getElementById("reactor2").className = "fade-out";
                 btnElement.value = "ON";
             }
             else {
                 document.getElementById("eyeRblur").className = "fade-in";
                 document.getElementById("eyeLblur").className = "fade-in";
+                document.getElementById("reactor1").className = "fade-in";
+                document.getElementById("reactor2").className = "fade-in";
                 btnElement.value = "OFF";
             }
         };
